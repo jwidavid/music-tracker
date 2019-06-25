236d1e96f49939aca841723646d41a504c96c317
Jared David <jwidavid72@gmail.com>
2019-06-25 15:05:05 -0400

    Bands page created

---
+++ resources/views/base.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title', 'Page') | Music Tracker</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Bands</a>
+            <a class="p-2 text-dark" href="#">Albums</a>
+        </nav>
+    </div>
+
+    @yield('content_main')
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,22 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
+
+    $bands = [
+        ['name' => 'Glen Miller', 'albums' => 3],
+        ['name' => 'Frank Sinatra', 'albums' => 10],
+        ['name' => 'Mills Brothers', 'albums' => 4],
+        ['name' => 'Nat King Cole', 'albums' => 3]
+    ];
+    
+    return view('bands', [
+        'bands' => $bands
+    ]);
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
